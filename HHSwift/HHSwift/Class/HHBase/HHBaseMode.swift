ca5df104ab291709810ae9dff68a6c749ba53609
Mac <mac@Macs-MacBook-Pro.local>
2018-04-30 16:24:35 +0800

    删除H-01

--- HHSwift/HHSwift/Class/HHBase/HHBaseMode.swift
+++ HHSwift/HHSwift/Class/HHBase/HHBaseMode.swift
@@ -18,6 +18,14 @@ class HHBaseMode: NSObject {
     var data : String
     
     
+    /*!  Swift命名空间 - 就是项目名称
+     默认 所有类都是共享的，可以直接访问 不需要import
+     所有对象的属性是var 也可以直接访问
+     */
+    //给自己的属性分配空间并且设置初始值
+    //调用父类的构造函数 给父类属性分配空间设置初始值
+    //NSObject 没有属性 只有isa
+    
     //Stored property 'name' without initial value prevents synthesized initializers
     
     //Property 'self.name' not initialized at implicitly generated super.init call
